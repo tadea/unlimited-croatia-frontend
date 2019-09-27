36ca1fc456a1f943ded202ad66102be0037b28c7
Tadea <tsimunoviczg@gmail.com>
2019-09-27 14:36:34 -0400

    setup and created store

--- src/App.js
+++ src/App.js
@@ -1,26 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  componentDidMount() {
+    fetch("http://localhost:3001/api/v1/locations", {
+      method: "GET"
+    })
+      .then(response => response.json())
+      .then(data => console.log(data));
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">App</header>
+      </div>
+    );
+  }
 }
 
 export default App;
