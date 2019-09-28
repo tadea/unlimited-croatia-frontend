9d2a3c598e47daf739013a26ebb522dff2979f41
Tadea <tsimunoviczg@gmail.com>
2019-09-28 11:28:34 -0400

    add input form for Locations

--- src/components/LocationsForm.js
+++ src/components/LocationsForm.js
@@ -1,8 +1,35 @@
 import React from "react";
 
 class LocationsForm extends React.Component {
+  state = {
+    name: "",
+    city: ""
+  };
+
   render() {
-    return <div>LocationsForm</div>;
+    return (
+      <div>
+        <form onSubmit={this.handleSubmit}>
+          <label>Region Name</label>
+          <input
+            placeholder="region name"
+            value={this.state.name}
+            name="name"
+            type="text"
+            onChange={this.handleChange}
+          />
+          <label>City Name</label>
+          <input
+            placeholder="city name"
+            value={this.state.city}
+            name="city"
+            type="text"
+            onChange={this.handleChange}
+          />
+          <input type="submit" value="Submit" />
+        </form>
+      </div>
+    );
   }
 }
 
