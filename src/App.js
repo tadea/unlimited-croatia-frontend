8334a260ca39e760b00e6aac2791fcc27362eda5
Tadea <tsimunoviczg@gmail.com>
2019-09-28 10:47:12 -0400

    removed mapStateToProps, add LocationConatiner

--- src/App.js
+++ src/App.js
@@ -1,19 +1,14 @@
 import React from "react";
 import "./App.css";
+import LocationsContainer from "./containers/LocationsContainer.js";
 
 class App extends React.Component {
-  componentDidMount() {
-    fetch("http://localhost:3001/api/v1/locations", {
-      method: "GET"
-    })
-      .then(response => response.json())
-      .then(data => console.log(data));
-  }
-
   render() {
     return (
       <div className="App">
-        <header className="App-header">App</header>
+        <header className="App-header">
+          <LocationsContainer />
+        </header>
       </div>
     );
   }
--- src/index.js
+++ src/index.js
@@ -3,12 +3,15 @@ import ReactDOM from "react-dom";
 import { createStore, applyMiddleware, compose } from "redux";
 import thunk from "redux-thunk";
 import { Provider } from "react-redux"; //any component that we wrap will have acces to our store
-
+import locationReducer from "./reducers/locationReducer";
 import App from "./App";
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
-let store = createStore(reducer, composeEnhancers(applyMiddleware(thunk)));
+let store = createStore(
+  locationReducer,
+  composeEnhancers(applyMiddleware(thunk))
+);
 
 ReactDOM.render(
   <Provider store={store}>
