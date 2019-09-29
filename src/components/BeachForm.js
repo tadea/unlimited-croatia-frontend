313d96312a67f40e091ec8b829963ca298c2ec27
Tadea <tsimunoviczg@gmail.com>
2019-09-28 21:43:41 -0400

    created form for Beach

--- src/components/BeachForm.js
+++ src/components/BeachForm.js
@@ -1,12 +1,56 @@
 import React from 'react'
+import { connect } from 'react-redux'
 
 class BeachForm extends React.Component {
-    state = {}
+    state = {
+        name: '',
+        region: 'Istria',
+        summary: ''
+    }
+
+    handleChange = event => {
+        this.setState({
+            [event.target.name]: event.target.value
+        });
+    };
+
+    handleSubmit = event => {
+        event.preventDefault();
+
+    };
+
+
     render() {
         return (
-            <div>BeachForm</div>
+            <div>
+                <form onSubmit={this.handleSubmit}>
+                    <label>Beach Name</label>
+                    <input
+                        placeholder="name"
+                        value={this.state.name}
+                        name="name"
+                        type="text"
+                        onChange={this.handleChange}
+                    />
+                    <label>Choose Region</label>
+                    <select name="region" value={this.state.region} onChange={this.handleChange}>
+                        <option>Istria</option>
+                        <option>Kvarner</option>
+                        <option>Dalmatia</option>
+                    </select>
+                    <label>Summary</label>
+                    <input
+                        placeholder="summary"
+                        value={this.state.summary}
+                        name="summary"
+                        type="text"
+                        onChange={this.handleChange}
+                    />
+                    <input type="submit" value="Create" />
+                </form>
+            </div>
         );
     }
 }
 
-export default BeachForm;
+export default connect()(BeachForm);
