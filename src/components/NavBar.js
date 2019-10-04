81130354b834b7e47aeda40d4ad566c9e6a740ce
Tadea <tsimunoviczg@gmail.com>
2019-10-04 15:14:45 -0400

    add css class to navBar and import App.scss

---
+++ src/components/NavBar.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import "../App.scss";
+import { Link } from 'react-router-dom'
+
+
+
+export default (props) => {
+    return (
+        <nav className="navbar">
+            <div className="nav-center">
+                <div className="nav-header">
+                    <Link to="/">Home</Link>
+                    <Link to="/about">About</Link>
+                    <Link to="/locations">Locations</Link>
+                    <Link to="/locations/new">Create Location</Link>
+                </div>
+
+            </div>
+
+        </nav>
+    )
+
+};
+
+
+
+
+{/* <div className="NavBar">
+    <Link to="/">Home</Link>
+    <Link to="/about">About</Link>
+    <Link to="/locations">Locations</Link>
+</div> */}
