74db7b878680316c6175473397cecf882f5df58c
Tadea <tsimunoviczg@gmail.com>
2019-10-04 15:24:07 -0400

    import index.scss and app.scss

--- src/App.js
+++ src/App.js
@@ -1,16 +1,24 @@
 import React from "react";
-import "./App.css";
+import "./index.scss";
+import "./App.scss";
+import Home from "./components/Home.js";
+import About from "./components/About.js";
 import LocationsContainer from "./containers/LocationsContainer.js";
+import { Route, Switch } from "react-router-dom";
+import NavBar from "./components/NavBar.js"
 
 class App extends React.Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <LocationsContainer />
-        </header>
+      <div>
+        <NavBar />
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route exact path="/about" component={About} />
+          <Route excat path="/locations" component={LocationsContainer} />
+        </Switch>
       </div>
-    );
+    )
   }
 }
 
