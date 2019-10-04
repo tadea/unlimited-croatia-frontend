0b6ef15e2621389e67e753c14752d84fe20d84ad
Tadea <tsimunoviczg@gmail.com>
2019-10-04 15:15:28 -0400

    add css class to About and Home and import App.scss

---
+++ src/components/About.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import "../App.scss";
+
+const About = () => {
+    return (
+        <div className="about">
+            <h3>It’s high time you made your way to the paradise that is Croatia.
+                Beaches with crystal clear water combined with impressively well-preserved
+                old towns will make your trip to Croatia one you’ll never forget
+            </h3>
+        </div>
+
+    )
+}
+
+export default About
---
+++ src/components/Home.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import "../App.scss";
+
+const Home = () => {
+    return (
+        <div class="banner">
+            <div className="row">
+                <h1>UNLIMITED CROATIA</h1>
+                <p>Full of Life</p>
+            </div>
+        </div>
+
+
+    )
+}
+
+export default Home
