bf65f600b3acd4d2e983b3fb229128c7596a4abe
Tadea <tsimunoviczg@gmail.com>
2019-11-25 15:13:26 -0500

    removed conosle.logs

--- src/actions/addLocation.js
+++ src/actions/addLocation.js
@@ -1,6 +1,5 @@
 export const addLocation = data => {
   return dispatch => {
-    console.log('C');
     fetch("http://localhost:3001/api/v1/locations", {
       headers: {
         "Content-Type": "application/json"
@@ -13,12 +12,10 @@ export const addLocation = data => {
         if (location.error) {
           alert(location.error)
         } else {
-          console.log('D')
           dispatch({ type: "ADD_LOCATION", payload: location })
         }
       })
   };
-  console.log('E')
 };
 
 export default addLocation;
