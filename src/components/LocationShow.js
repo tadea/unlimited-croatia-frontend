dfc03a6fad2aeae325d093f93a076571026f5fb0
Tadea <tsimunoviczg@gmail.com>
2019-12-11 20:20:36 -0500

    clean up code

--- src/components/LocationShow.js
+++ src/components/LocationShow.js
@@ -1,21 +1,19 @@
 import React from "react";
 import BeachesContainer from '../containers/BeachesContainer.js'
 import "../App.scss";
-import { Link } from "react-router-dom"
+
 
 class LocationShow extends React.Component {
 
-
-  // let location = props.locations.filter(location => location.id == props.match.params.id)[0]
   render() {
     let location = this.props.locations.filter(location => location.id == this.props.match.params.id)[0];
-
     return (
 
       <div className="locationName">
         <h1><i className="fas fa-umbrella-beach"></i>
-          <br></br>
+          <br />
           {location ? location.name : null}-{location ? location.city : null}</h1>
+
         <div>
           <BeachesContainer location={location} />
         </div>
