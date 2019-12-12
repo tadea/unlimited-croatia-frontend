d2b64f0c644564d139a4f7ed2fd157e116550dd9
Tadea <tsimunoviczg@gmail.com>
2019-12-11 20:19:55 -0500

    separate Locations in LocationCard component

--- src/components/LocationCard.js
+++ src/components/LocationCard.js
@@ -2,12 +2,24 @@ import React from "react";
 import { Link } from 'react-router-dom'
 
 
+
 class LocationCard extends React.Component {
+
+
+    state = { likes: 0 }
+
+    onLike = () => {
+        this.setState((state) => {
+            return { likes: state.likes + 1 }
+        })
+    }
 
     render() {
         return (
-            <div className="LocationCard" key={this.props.location.id} onVote={this.handleEvent}>
+            <div className="LocationCard" key={this.props.location.id}>
                 <h4><Link to={`/locations/${this.props.location.id}`}>{this.props.location.city} </Link></h4>
+                <button onClick={() => { this.onLike() }}>Like</button>
+                <p>Likes: {this.state.likes}</p>
             </div>
         );
     }
