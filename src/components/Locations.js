37cf05f6a7240ab862df966d9c1c773e12f3106e
Tadea <tsimunoviczg@gmail.com>
2019-12-11 20:21:18 -0500

    refactor and import LocationCard component

--- src/components/Locations.js
+++ src/components/Locations.js
@@ -1,16 +1,16 @@
 
 import React from "react";
-import { Link } from 'react-router-dom'
 import LocationCard from './LocationCard'
 
 
 class Location extends React.Component {
 
   render() {
+
     return (
       <div>
         {this.props.locations.map(location =>
-          <LocationCard location={location}></LocationCard>
+          <LocationCard key={location.id} location={location}></LocationCard>
         )}
       </div>
     );
