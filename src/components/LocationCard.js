f43abd2fa39c0f22086c5f335a79e0eb7fb013b8
Tadea <tsimunoviczg@gmail.com>
2020-04-03 22:38:39 -0400

    changed style for like

--- src/components/LocationCard.js
+++ src/components/LocationCard.js
@@ -1,28 +1,34 @@
 import React from "react";
-import { Link } from 'react-router-dom'
-
-
+import { Link } from "react-router-dom";
 
 class LocationCard extends React.Component {
+  state = { likes: 0 };
 
+  onLike = () => {
+    this.setState((state) => {
+      return { likes: state.likes + 1 };
+    });
+  };
 
-    state = { likes: 0 }
+  render() {
+    return (
+      <div className="LocationCard" key={this.props.location.id}>
+        <h4>
+          <Link to={`/locations/${this.props.location.id}`}>
+            {this.props.location.city}{" "}
+          </Link>
+        </h4>
+        <button
+          onClick={() => {
+            this.onLike();
+          }}
+        >
+          &hearts;
+        </button>
+        <p>Likes: {this.state.likes}</p>
+      </div>
+    );
+  }
+}
 
-    onLike = () => {
-        this.setState((state) => {
-            return { likes: state.likes + 1 }
-        })
-    }
-
-    render() {
-        return (
-            <div className="LocationCard" key={this.props.location.id}>
-                <h4><Link to={`/locations/${this.props.location.id}`}>{this.props.location.city} </Link></h4>
-                <button onClick={() => { this.onLike() }}>Like</button>
-                <p>Likes: {this.state.likes}</p>
-            </div>
-        );
-    }
-};
-
-export default LocationCard; 
+export default LocationCard;
